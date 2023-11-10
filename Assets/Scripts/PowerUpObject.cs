08f0248e3c26bf5f4f8a95ab4c2b727a7735732c
BananabroJR <emiller@student.neumont.edu>
2023-11-10 08:59:02 -0700

    Made a simple scritpable object script

---
+++ Assets/Scripts/PowerUpObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpObject : ScriptableObject
+{
+    public float verticleAcceloration;
+    public float horizontalAcceloration;
+    public float speed;
+
+}
