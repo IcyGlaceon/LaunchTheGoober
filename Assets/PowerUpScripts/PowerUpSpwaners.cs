5ffbbf2559c3ab7594471a691582f488af036119
BananabroJR <emiller@student.neumont.edu>
2023-12-06 01:07:14 -0700

    Powerups Spawn

--- Assets/PowerUpScripts/PowerUpSpwaners.cs
+++ Assets/PowerUpScripts/PowerUpSpwaners.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PowerUpSpwaners : MonoBehaviour
 {
+    public GameObject fan;
+    public GameObject booster;
+    public GameObject UpAndRightBooster;
+    
+    public Transform fanSpawner;
+    public Transform boosterSpawner;
+    public Transform UpAndRightBoosterSpawner;
+
+    private GameObject newFan;
+    private GameObject newBooster;
+    private GameObject newUpAndRightBooster;
+
+
+    private bool doesFanExist = false;
+    private bool doesBoosterExist = false;
+    private bool doesUpBoosterExist = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +31,51 @@ public class PowerUpSpwaners : MonoBehaviour
     void Update()
     {
         
+
+        if(!doesFanExist)
+        {
+            newFan = Instantiate(fan);
+            newFan.transform.position = fanSpawner.position;
+            doesFanExist= true;
+        }
+        if(newFan.transform.position.x < gameObject.transform.position.x)
+        {
+            Destroy(newFan);
+            doesFanExist= false;
+        }
+
+        if (!doesBoosterExist)
+        {
+            newBooster = Instantiate(booster);
+            newBooster.transform.position = boosterSpawner.position;
+            doesBoosterExist = true;
+        }
+        if (newBooster.transform.position.x < gameObject.transform.position.x)
+        {
+            Destroy(newBooster);
+            doesBoosterExist = false;
+        }
+
+        if (!doesUpBoosterExist)
+        {
+            newUpAndRightBooster = Instantiate(UpAndRightBooster);
+            newUpAndRightBooster.transform.position = UpAndRightBoosterSpawner.position;
+            doesUpBoosterExist = true;
+        }
+        if (newUpAndRightBooster.transform.position.x < gameObject.transform.position.x)
+        {
+            Destroy(newUpAndRightBooster);
+            doesUpBoosterExist = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Grounded")
+        {
+            doesBoosterExist= true;
+            doesFanExist = true;
+            doesUpBoosterExist = true; 
+        }
     }
 }
